9087259dacb5815f020b42d1caefe7483c959b3c
Metodi Obetsanov <htc.sens1@gmail.com>
2018-12-06 16:39:08 +0200

    Project Creation
    Project created

---
+++ src/Data/AlpineClubBansko.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlpineClubBansko.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlpineClubBansko.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
