a2fb118109e0cee24048809ea44b7d43a5301a85
Metodi Obetsanov <htc.sens1@gmail.com>
2019-01-10 12:37:58 +0200

    Update
    HTTPS used and css fixes

--- src/Web/AlpineClubBansko.Web/Startup.cs
+++ src/Web/AlpineClubBansko.Web/Startup.cs
@@ -106,7 +106,7 @@ namespace AlpineClubBansko.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseHttpsRedirection();
             app.UseSeedDataMiddleware();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
